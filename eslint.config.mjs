d98f5fe3123076cb3575788f71709c43220b0d34
Alexander Skvorchevskiy <43242733+notsecret32@users.noreply.github.com>
2024-12-02 02:22:54 +0300

    feat: added basic project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    ignores: ['**/dist/**', '**/node_modules/**'],
+  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
